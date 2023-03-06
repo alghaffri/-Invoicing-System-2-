110be3ec45280b5ff22dd2846cfe91a298797b66
alghaffri <121156756+alghaffri@users.noreply.github.com>
2023-03-06 15:13:41 +0400

    create Shop class

---
+++ src/Shop.java
@@ -0,0 +1,44 @@
+public class Shop {
+	private String  shopName;
+	private int telephone;
+	private String Fax;
+	private String  email; 
+	   
+
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+
+	public int getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getFax() {
+		return Fax;
+	}
+
+	public void setFax(String fax) {
+		Fax = fax;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static void createShopTable() {
+	}
+}
+	
